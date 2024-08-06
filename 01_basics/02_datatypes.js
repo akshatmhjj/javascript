da39efbb37a5ec5016c010cd503d22c181af0796
akshatmhjj <akshatmahajan32@gmail.com>
2024-08-06 12:43:37 +0000

    Datatypes in java

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version of codes
+
+// alert (3+3) // we are using node JS and not browser so alert will not show output
+
+console.log(3+3)
+
+console.log("akshat") // code readability should be high
+
+let name = "akshat" // string
+let age = 20 // number
+let isLoggedIn = false // boolean
+let state;
+
+// number => 2 to the power of 53
+// bigint => for too large numbers
+// string => ""
+// boolean => true/false
+// null => standalone value (representation of empty value)
+// undefined => variable is declared but value is not assigned
+// symbol => unique
+
+// object
+
+console.log(typeof "akshat") // typeof is used for finding the data type of the variable or value
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
