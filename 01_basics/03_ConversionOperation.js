a7d717d32bb69ed7618564d7662fc39687fc8f6a
akshatmhjj <akshatmahajan32@gmail.com>
2024-08-07 09:30:59 +0000

    new

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -1,2 +1,18 @@
-let score = 33 
+let score = "33abc" 
 
+// console.log(typeof score)
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// Basic Conversions of datatype in output
+//"33" => 33
+// "33abc" => NaN (not a number)
+// true => 1; false => 0
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+Console.log(booleanIsLoggedIn);
