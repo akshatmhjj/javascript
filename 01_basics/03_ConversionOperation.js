552f6998de0e12ce7323c0bab81d2de84807572e
akshatmhjj <akshatmahajan32@gmail.com>
2024-08-07 09:39:31 +0000

    03

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -11,8 +11,17 @@ let valueInNumber = Number(score)
 // "33abc" => NaN (not a number)
 // true => 1; false => 0
 
-
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-Console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" = false
+// "akshat" => true
+
+let someNumber = 33 
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
