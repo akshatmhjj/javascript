b78abe651738c742a74c2b6fd8341afed43cded2
akshatmhjj <akshatmahajan32@gmail.com>
2024-08-07 12:39:00 +0000

    new

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,7 @@
+// Primitive
+
+// Types : String, Number, Boolean, Null, Undefined, Symbol (used for making a value unique), BigInt (used for too large value)
+
+// Non-Primitive (Reference)
+
+// Array, Objects, Function
