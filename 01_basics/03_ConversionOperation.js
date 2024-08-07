b642f9bd446b076842255bff698f1cb34b2dc90a
akshatmhjj <akshatmahajan32@gmail.com>
2024-08-07 10:12:53 +0000

    conversion and opr

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -23,5 +23,41 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33 
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// ****************************** OPERATIONS ******************************
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(10/2);
+// console.log(2%3);
+// Basic Math Operations
+
+let str1 = "Hello"
+let str2 = " Akshat"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2 + 2 );
+// console.log(2 + 2 + "1");
+// console.log(2 + "1" + 2);
+
+// console.log( (3 + 4) * 5 ); // Use parenthesis (brackets) between multiple operations due to code readability
+
+// console.log(+true); // true will be given as true and +true will be considered incremented and will be given as 1
+// console.log(+""); //output will be 0
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+
